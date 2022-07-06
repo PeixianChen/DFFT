3489620f68ce4b3f709afeb65440c6e0ce213c27
Peixian Chen <pealing@foxmail.com>
2022-07-06 13:55:48 +0800

    Update dfft.py

--- configs/dfft/dfft.py
+++ configs/dfft/dfft.py
@@ -60,8 +60,6 @@ optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), wei
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
                                                  'relative_position_bias_table': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.)}))
-# lr_config = dict(step=[16, 21, 26])
-# runner = dict(type='EpochBasedRunnerAmp', max_epochs=36)
 # lr_config = dict(step=[8, 11])
 # runner = dict(type='EpochBasedRunnerAmp', max_epochs=12)
 lr_config = dict(step=[27, 33])
